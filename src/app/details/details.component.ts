be473965705c9e1891161098d9078382c29d07dd
Alexandre Leduey <alexandre.leduey@kappalys.com>
2021-02-05 15:45:34 +0100

    Ajoute les menus actions

--- src/app/details/details.component.ts
+++ src/app/details/details.component.ts
@@ -14,6 +14,9 @@ export class DetailsComponent {
   selectedRessource?: Ressources;
   current: number = 1;
 
+  startMenu: boolean = true;
+  createMenu: boolean = false;
+
   constructor(private ressourcesService: RessourcesService, private jeuService: JeuService) { 
     this.jeuService.currentPlayer.subscribe((value) => {
       this.current = value;
@@ -34,8 +37,7 @@ export class DetailsComponent {
   }
 
   creer() {
-    this.jeuService.addNewRessources();
-    this.jeuService.currentPlayer = this.jeuService.nextCurrentPlayer();
+    this.setCreateMenu();
   }
 
   attaquer() {
@@ -46,5 +48,58 @@ export class DetailsComponent {
   deplacer() {
     this.jeuService.addNewRessources();
     this.jeuService.currentPlayer = this.jeuService.nextCurrentPlayer();
+  }
+
+  // LES MENUS ACTIONS
+  setStartMenu() {
+    this.startMenu = true;
+    this.createMenu = false;
+  }
+
+  setCreateMenu() {
+    this.startMenu = false;
+    this.createMenu = true;
+  }
+
+  // ACTIONS CREATE
+  setPuissance(coef = 1) {
+    alert(coef);
+  }
+
+  canBuy(coef: number) {
+    switch (coef) {
+      case 1:
+        if (
+          this.jeuService.joueurs[this.current].getRessourceById(1) === 5 &&
+          this.jeuService.joueurs[this.current].getRessourceById(3) === 3
+        ) {
+          alert(1);
+          return true;
+        }
+        return false;
+
+      case 5:
+        if (
+          this.jeuService.joueurs[this.current].getRessourceById(2) === 4 &&
+          this.jeuService.joueurs[this.current].getRessourceById(4) === 3 &&
+          this.jeuService.joueurs[this.current].getRessourceById(5) === 2
+        ) {
+          return true;
+        }
+        return false;
+
+      case 15:
+        if (
+          this.jeuService.joueurs[this.current].getRessourceById(2) === 6 &&
+          this.jeuService.joueurs[this.current].getRessourceById(4) === 5 &&
+          this.jeuService.joueurs[this.current].getRessourceById(6) === 3
+        ) {
+          return true;
+        }
+        return false;
+
+      default:
+        return false;
+    }
   }
 }
